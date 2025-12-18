2323b9a74d2095d09ade916bb0d3c0be50e9f255
EA5JTT <joanpao@gmail.com>
2025-12-18 16:40:30 +0100

    Add copyright and license information
    Added copyright notice and licensing information.

--- GPS_Display.ino
+++ GPS_Display.ino
@@ -12,7 +12,22 @@
 *  - Puede salir por serie y/o por display segun como se quiera
 *  - La Hora es UTC
 ******************************************/
-
+/*
+ * Copyright (C) 2025 Juan Pablo Sanchez EA5JTT
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 // No hace falta en el proyecto pues está en la biblioteca de Arduino IDE
 #include <SPI.h>
 #include <Wire.h>
